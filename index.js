91a4fc3a7a65dae1e3418a1389f9d2750a4e0eea
Liam <liam.bowers@adp.com>
2017-08-14 08:28:58 -0400

    update pdf assembly script

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const markdownpdf = require('markdown-pdf');
-const fs = require('fs');
+var markdownpdf = require('markdown-pdf');
 
-fs.createReadStream('./README.md')
-  .pipe(markdownpdf())
-  .pipe(fs.createWriteStream('./RESUME.pdf'))
+const inputs = [
+  'sections/00-HEADER.md',
+  'sections/01-PROFILE.md',
+  'sections/02-EDUCATION.md',
+  'sections/03-EXPERIENCE.md',
+  'sections/04-ACTIVITIES.md',
+];
+const output = 'RESUME.pdf';
+
+const options = {
+  cssPath: 'styles/pdf.css',
+};
+
+markdownpdf().concat.from(inputs, options).to(output, () => {
+  console.log('Created', output)
+});
