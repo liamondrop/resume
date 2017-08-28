633e7399c48c4e4db86a2ef95ca1c971a686d29b
Liam <liam.bowers@adp.com>
2017-08-28 17:35:44 -0400

    update build script

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ var markdownpdf = require('markdown-pdf');
 
 const inputs = [
   'sections/00-HEADER.md',
-  'sections/01-PROFILE.md',
+  'sections/06-SKILLS.md',
   'sections/02-EDUCATION.md',
   'sections/03-EXPERIENCE.md',
   'sections/04-ACTIVITIES.md',
 ];
+
 const output = 'RESUME.pdf';
 
 const options = {
   cssPath: 'styles/pdf.css',
 };
 
-markdownpdf().concat.from(inputs, options).to(output, () => {
+markdownpdf(options).concat.from(inputs).to(output, () => {
   console.log('Created', output)
 });
